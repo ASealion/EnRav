f674454814c93c21dcefac32a0fa0d89e240784f
Mike <mail@sealion.de>
2018-10-09 12:53:44 +0200

    Move player function into separate class

--- src/enrav_main.ino
+++ src/enrav_main.ino
@@ -2,7 +2,8 @@
 #include <SPI.h>
 #include <WiFi.h>
 #include "vs1053_ext.h"
-#include <MFRC522.h>
+#include "MFRC522.h"
+#include "mp3player.h"
 
 #ifdef ARDUINO_ARCH_ESP32
     #include "esp32-hal-log.h"
@@ -34,74 +35,11 @@
 
 int volume=15;
 
-VS1053  Mp3Player(VS1053_CS, VS1053_DCS, VS1053_DREQ);
-MFRC522 mfrc522(MFRC522_CS, MFRC522_RST);  // Create MFRC522 instance
+//VS1053      player(VS1053_CS, VS1053_DCS, VS1053_DREQ);
+MFRC522     mfrc522(MFRC522_CS, MFRC522_RST);  // Create MFRC522 instance
+Mp3player   MyPlayer(VS1053_CS, VS1053_DCS, VS1053_DREQ);
 
-//----------------------------------------
-
-typedef enum {
-    CMD_UNKNOWN,
-    CMD_PLAY_FILE,
-    CMD_STOP, 
-    CMD_VOL_UP,
-    CMD_VOL_DOWN,
-} PlayerCommand_e;
-
-typedef struct {
-    PlayerCommand_e Command;
-    char           *pFileToPlay;
-} PlayerControlMessage_s;
-
-QueueHandle_t xPlayerQueue;
-//----------------------------------------
-
-void mp3Player( void * parameter ) {
-    PlayerControlMessage_s   PlayerControlMessage;
-    QueueHandle_t           *pPlayerQueue = (QueueHandle_t *) parameter;
-    
-
-
-    Mp3Player.begin();
-    Mp3Player.setVolume(volume);
-    Mp3Player.connecttoSD("/01.mp3"); // SD card
-
-    //mp3.begin();
-    //mp3.setVolume(volume);
-    //mp3.connecttoSD("/01.mp3"); // SD card
-    //mp3.connecttohost("edge.audio.3qsdn.com/senderkw-mp3");
-    //mp3.connecttohost("https://icecast-qmusicnl-cdp.triple-it.nl/Qmusic_nl_fouteuur_96.mp3");
-
-    while (1)
-    {
-        Mp3Player.loop();
-
-        if( xQueueReceive( *pPlayerQueue, &(PlayerControlMessage), ( TickType_t ) 50 ) ) 
-        {
-            ESP_LOGV(TAG, "Received Command %u", PlayerControlMessage.Command);
-
-            if (PlayerControlMessage.Command == CMD_PLAY_FILE) 
-            {
-                //make sure the file exists
-                if (PlayerControlMessage.pFileToPlay != NULL)
-                {
-                    ESP_LOGD(TAG, "Received Path %s", PlayerControlMessage.pFileToPlay);
-
-                    Mp3Player.connecttoSD(PlayerControlMessage.pFileToPlay); 
-
-                    free(PlayerControlMessage.pFileToPlay);
-
-                }
-            }
-            else if (PlayerControlMessage.Command == CMD_STOP) 
-            {
-                ESP_LOGD(TAG, "Received stop");
-                Mp3Player.stop_mp3client();
-            }
-        }
-
-        delay(50);
-    }
-}
+QueueHandle_t *pPlayerQueue;
 
 //The setup function is called once at startup of the sketch
 void setup() {
@@ -116,6 +54,10 @@ void setup() {
 
     SD.begin(SDCARD_CS);
 
+    pPlayerQueue = MyPlayer.begin();
+    
+    //pPlayerQueue = MyPlayer.getQueue();
+
     // WiFi.disconnect();
     // WiFi.mode(WIFI_STA);
     // WiFi.begin(ssid.c_str(), password.c_str());
@@ -125,21 +67,6 @@ void setup() {
 
     //------------------------------------------
 
-    xPlayerQueue = xQueueCreate( 5, sizeof( PlayerControlMessage_s ) );
-
-    if( xPlayerQueue == NULL )
-    {
-        ESP_LOGE(TAG, "Could not create player queue");
-        while(1);
-    }
-    //create the task that will handle the playback
-    xTaskCreate(
-                    mp3Player,        /* Task function. */
-                    "MP3 Player",     /* String with name of task. */
-                    4* 1024,          /* Stack size in words. */
-                    &xPlayerQueue,    /* Parameter passed as input of the task */
-                    1,                /* Priority of the task. */
-                    NULL);            /* Task handle. */
 
 }
 
@@ -197,7 +124,7 @@ void loop()
 
     if (myMessage.Command != CMD_UNKNOWN) 
     {
-        if (xQueueSend( xPlayerQueue, &myMessage, ( TickType_t ) 0 ) )
+        if (xQueueSend( *pPlayerQueue, &myMessage, ( TickType_t ) 0 ) )
         {
             ESP_LOGD(TAG, "send to queue successfull");
         } else {
@@ -209,19 +136,4 @@ void loop()
     }
     mfrc522.PICC_HaltA();
     mfrc522.PCD_StopCrypto1();
-    // PlayerControlMessage_s myMessage;
-
-    // myMessage.Command       = CMD_PLAY_FILE;
-    // myMessage.pFileToPlay   = (char *) malloc (16 * sizeof(char));
-    // strncpy(myMessage.pFileToPlay, "/02.mp3", 16); 
-
-    // if (xQueueSend( xPlayerQueue, &myMessage, ( TickType_t ) 0 ) )
-    // {
-    //     ESP_LOGD(TAG, "send to queue successfull");
-    // } else {
-    //     ESP_LOGD(TAG, "send to queue failed");
-
-    //     //if the send failed, we must do the job
-    //     free(myMessage.pFileToPlay);
-    // }
 }
