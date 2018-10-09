ab5fb68cd3fa4768ed2c9e080673c5fe84962500
EnRav <developer@mail.enrav.de>
2018-10-09 12:53:44 +0200

    Move player function into separate class

---
+++ lib/Mp3Player/src/mp3player.cpp
@@ -0,0 +1,104 @@
+
+#include "mp3player.h"
+
+Mp3player::Mp3player(uint8_t _cs_pin = 25, uint8_t _dcs_pin = 26, uint8_t _dreq_pin = 32)
+{
+    m_pPlayer = new VS1053(_cs_pin, _dcs_pin, _dreq_pin);
+}
+
+Mp3player::~Mp3player()
+{
+
+}
+
+
+QueueHandle_t *Mp3player::begin( void )
+{
+    m_PlayerQueue = xQueueCreate( 5, sizeof( PlayerControlMessage_s ) );
+
+    if( m_PlayerQueue == NULL )
+    {
+        ESP_LOGE(TAG, "Could not create player queue");
+        return NULL;
+    }
+    //create the task that will handle the playback
+    xTaskCreate(
+                    TaskFunctionAdapter,        /* Task function. */
+                    "MP3 Player",       	    /* String with name of task. */
+                    4* 1024,                    /* Stack size in words. */
+                    this,                       /* Parameter passed as input of the task */
+                    1,                          /* Priority of the task. */
+                    &m_handle);                 /* Task handle. */
+
+    return &(this->m_PlayerQueue);
+}
+
+
+void Mp3player::TaskFunctionAdapter(void *pvParameters)
+{
+    Mp3player *mp3player = static_cast<Mp3player *>(pvParameters);
+
+    mp3player->Run();
+
+    mp3player->CleanUp();
+
+    vTaskDelete(mp3player->m_handle);
+}
+
+
+
+void Mp3player::Run( void ) {
+    PlayerControlMessage_s   PlayerControlMessage;
+
+    m_pPlayer->begin();
+    m_pPlayer->setVolume(15);
+    m_pPlayer->connecttoSD("/01.mp3"); // SD card
+
+    //mp3.begin();
+    //mp3.setVolume(volume);
+    //mp3.connecttoSD("/01.mp3"); // SD card
+    //mp3.connecttohost("edge.audio.3qsdn.com/senderkw-mp3");
+    //mp3.connecttohost("https://icecast-qmusicnl-cdp.triple-it.nl/Qmusic_nl_fouteuur_96.mp3");
+
+    while (true)
+    {
+        m_pPlayer->loop();
+
+        if( xQueueReceive( m_PlayerQueue, &(PlayerControlMessage), ( TickType_t ) 50 ) ) 
+        {
+            ESP_LOGV(TAG, "Received Command %u", PlayerControlMessage.Command);
+
+            if (PlayerControlMessage.Command == CMD_PLAY_FILE) 
+            {
+                //make sure the file exists
+                if (PlayerControlMessage.pFileToPlay != NULL)
+                {
+                    ESP_LOGD(TAG, "Received Path %s", PlayerControlMessage.pFileToPlay);
+
+                    m_pPlayer->connecttoSD(PlayerControlMessage.pFileToPlay); 
+
+                    free(PlayerControlMessage.pFileToPlay);
+
+                }
+            }
+            else if (PlayerControlMessage.Command == CMD_STOP) 
+            {
+                ESP_LOGD(TAG, "Received stop");
+                m_pPlayer->stop_mp3client();
+            }
+        }
+
+        delay(50);
+    };
+}
+
+
+void Mp3player::CleanUp( void )
+{
+
+}
+
+QueueHandle_t *Mp3player::getQueue( void )
+{
+    return &(this->m_PlayerQueue);
+}
---
+++ lib/Mp3Player/src/mp3player.h
@@ -0,0 +1,50 @@
+#ifndef _MP3_PLAYER
+    #define _MP3_PLAYER
+
+    #include "Arduino.h"
+    #include "SPI.h"
+    #include "SD.h"
+    #include "FS.h"
+
+    #include "vs1053_ext.h"
+
+
+
+    typedef enum {
+        CMD_UNKNOWN,
+        CMD_PLAY_FILE,
+        CMD_STOP, 
+        CMD_VOL_UP,
+        CMD_VOL_DOWN,
+    } PlayerCommand_e;
+
+    typedef struct {
+        PlayerCommand_e Command;
+        char           *pFileToPlay;
+    } PlayerControlMessage_s;
+
+    class Mp3player
+    {
+        public:
+            // Constructor.  Only sets pin values.  Doesn't touch the chip.  Be sure to call begin()!
+            Mp3player(uint8_t _cs_pin, uint8_t _dcs_pin, uint8_t _dreq_pin);
+            ~Mp3player();
+
+            QueueHandle_t *begin( void );
+
+            QueueHandle_t *getQueue( void );
+
+        private:
+            TaskHandle_t    m_handle;
+            QueueHandle_t   m_PlayerQueue;
+            VS1053          *m_pPlayer;
+
+
+            //
+            void Run( void );
+            void CleanUp( void );
+
+            static void TaskFunctionAdapter(void *pvParameters);
+    };
+
+#endif
