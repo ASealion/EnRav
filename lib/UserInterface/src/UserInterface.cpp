4da70a99ed558330d1a639264bc4387e4e3e4be9
EnRav <developer@mail.enrav.de>
2018-12-03 21:06:04 +0100

    add basic webserver example (not thread save)

--- lib/UserInterface/src/UserInterface.cpp
+++ lib/UserInterface/src/UserInterface.cpp
@@ -142,6 +142,7 @@ void UserInterface::Run( void ) {
                     }
                     break;
                 }
+
             case NoCard:
 
                 // Look for new cards
@@ -161,6 +162,7 @@ void UserInterface::Run( void ) {
                         m_lastCardUidSize = m_pRfReader->uid.size;
                         memcpy(m_lastCardUid, m_pRfReader->uid.uidByte, m_lastCardUidSize );
 
+                        //try to read the tag (and fill the NfcTag structure if successful)
                         if (this->ReadInformationFromTag() == true) 
                         {
                             ESP_LOGD(TAG, "Valid tag found");
@@ -551,3 +553,151 @@ void UserInterface::CleanUp( void )
 
 
                 // }
+
+                //     // Check for compatibility
+//     if (    piccType != MFRC522::PICC_TYPE_MIFARE_MINI
+//         &&  piccType != MFRC522::PICC_TYPE_MIFARE_1K
+//         &&  piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+//         Serial.println(F("This sample only works with MIFARE Classic cards."));
+//         goto CleanUp;
+//     }
+// {
+//     // In this sample we use the second sector,
+//     // that is: sector #1, covering block #4 up to and including block #7
+//     byte sector         = 1;
+//     byte blockAddr      = 4;
+//     byte dataBlock[]    = {
+//         0x01, 0x02, 0x03, 0x04, //  1,  2,   3,  4,
+//         0x05, 0x06, 0x07, 0x08, //  5,  6,   7,  8,
+//         0x09, 0x0a, 0xff, 0x0b, //  9, 10, 255, 11,
+//         0x0c, 0x0d, 0x0e, 0x0f  // 12, 13, 14, 15
+//     };
+//     byte trailerBlock   = 7;
+//     MFRC522::StatusCode status;
+//     byte buffer[18];
+//     byte size = sizeof(buffer);
+
+//     // Authenticate using key A
+//     Serial.println(F("Authenticating using key A..."));
+//     status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+//     if (status != MFRC522::STATUS_OK) {
+//         Serial.print(F("PCD_Authenticate() failed: "));
+//         Serial.println(mfrc522.GetStatusCodeName(status));
+//         return;
+//     }
+
+//     // Show the whole sector as it currently is
+//     Serial.println(F("Current data in sector:"));
+//     mfrc522.PICC_DumpMifareClassicSectorToSerial(&(mfrc522.uid), &key, sector);
+//     Serial.println();
+
+//     // Read data from the block
+//     Serial.print(F("Reading data from block ")); Serial.print(blockAddr);
+//     Serial.println(F(" ..."));
+//     status = mfrc522.MIFARE_Read(blockAddr, buffer, &size);
+//     if (status != MFRC522::STATUS_OK) {
+//         Serial.print(F("MIFARE_Read() failed: "));
+//         Serial.println(mfrc522.GetStatusCodeName(status));
+//     }
+//     Serial.print(F("Data in block ")); Serial.print(blockAddr); Serial.println(F(":"));
+//     dump_byte_array(buffer, 16); Serial.println();
+//     Serial.println();
+
+//     //prepare a dummy Tag
+//     memset(myTag.Target.DataPath, 0, sizeof(myTag.Target.DataPath));
+//     strcpy(myTag.Target.DataPath, "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod");
+
+//     ESP_LOGD(TAG, "Writing data into block 4..6, 8..10");
+
+//     ESP_LOGD(TAG, "writing info block 4 ..");
+//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(4, myTag.Information.Raw, 16);
+//     if (status != MFRC522::STATUS_OK) {
+//         Serial.print(F("MIFARE_Write(4) failed: "));
+//         Serial.println(mfrc522.GetStatusCodeName(status));
+//     }
+//     ESP_LOGD(TAG, "done");
+
+//     ESP_LOGD(TAG, "writing info block 5 ..");
+//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(5, myTag.Target.Raw[0], 16);
+//     if (status != MFRC522::STATUS_OK) {
+//         Serial.print(F("MIFARE_Write(5) failed: "));
+//         Serial.println(mfrc522.GetStatusCodeName(status));
+//     }
+//     ESP_LOGD(TAG, "done");
+
+//     ESP_LOGD(TAG, "writing info block 6 ..");
+//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(6, myTag.Target.Raw[1], 16);
+//     if (status != MFRC522::STATUS_OK) {
+//         Serial.print(F("MIFARE_Write(5) failed: "));
+//         Serial.println(mfrc522.GetStatusCodeName(status));
+//     }
+//     ESP_LOGD(TAG, "done");
+
+//     // Authenticate using key A
+//     Serial.println(F("Authenticating using key A..."));
+//     status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 10, &key, &(mfrc522.uid));
+//     if (status != MFRC522::STATUS_OK) {
+//         Serial.print(F("PCD_Authenticate() failed: "));
+//         Serial.println(mfrc522.GetStatusCodeName(status));
+//         return;
+//     }
+
+//     ESP_LOGD(TAG, "writing info block 8 ..");
+//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(8, myTag.Target.Raw[2], 16);
+//     if (status != MFRC522::STATUS_OK) {
+//         Serial.print(F("MIFARE_Write(8) failed: "));
+//         Serial.println(mfrc522.GetStatusCodeName(status));
+//     }
+//     ESP_LOGD(TAG, "done");
+
+//     ESP_LOGD(TAG, "writing info block 9 ..");
+//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(9, myTag.Target.Raw[3], 16);
+//     if (status != MFRC522::STATUS_OK) {
+//         Serial.print(F("MIFARE_Write(9) failed: "));
+//         Serial.println(mfrc522.GetStatusCodeName(status));
+//     }
+//     ESP_LOGD(TAG, "done");
+
+//             ESP_LOGD(TAG, "writing info block 10 ..");
+//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(10, myTag.Target.Raw[4], 16);
+//     if (status != MFRC522::STATUS_OK) {
+//         Serial.print(F("MIFARE_Write(10) failed: "));
+//         Serial.println(mfrc522.GetStatusCodeName(status));
+//     }
+//     ESP_LOGD(TAG, "done");
+    // PlayerControlMessage_s myMessage = { .Command = CMD_UNKNOWN };
+
+    // if (mfrc522.uid.size >= 2) 
+    // {
+    //     if ((mfrc522.uid.uidByte[0] == 0x04) && (mfrc522.uid.uidByte[1] == 0x65))
+    //     {
+    //         myMessage.Command       = CMD_PLAY_FILE;
+    //         myMessage.pFileToPlay   = (char *) malloc (16 * sizeof(char));
+    //         strncpy(myMessage.pFileToPlay, "/01.mp3", 16); 
+    //     }
+    //     else if ((mfrc522.uid.uidByte[0] == 0x04) && (mfrc522.uid.uidByte[1] == 0xE8))
+    //     {
+    //         myMessage.Command       = CMD_PLAY_FILE;
+    //         myMessage.pFileToPlay   = (char *) malloc (16 * sizeof(char));
+    //         strncpy(myMessage.pFileToPlay, "/02.mp3", 16); 
+    //     }
+    //     else if ((mfrc522.uid.uidByte[0] == 0x04) && (mfrc522.uid.uidByte[1] == 0x01))
+    //     {
+    //         myMessage.Command       = CMD_STOP;
+    //         myMessage.pFileToPlay   = NULL;
+    //     }
+    // }
+
+    // if (myMessage.Command != CMD_UNKNOWN) 
+    // {
+    //     if (xQueueSend( *pPlayerQueue, &myMessage, ( TickType_t ) 0 ) )
+    //     {
+    //         ESP_LOGD(TAG, "send to queue successfull");
+    //     } else {
+    //         ESP_LOGD(TAG, "send to queue failed");
+
+    //         //if the send failed, we must do the job
+    //         free(myMessage.pFileToPlay);
+    //     }
+    // }
+// }
--- src/enrav_main.ino
+++ src/enrav_main.ino
@@ -5,6 +5,11 @@
 #include "mp3player.h"
 #include "UserInterface.h"
 
+//----------------------------
+#include <WebServer.h>
+#include <ESPmDNS.h>
+//----------------------------
+
 #ifdef ARDUINO_ARCH_ESP32
     #include "esp32-hal-log.h"
 #else
@@ -40,6 +45,204 @@ Mp3player       MyPlayer(VS1053_CS, VS1053_DCS, VS1053_DREQ);
 
 QueueHandle_t   PlayerCommandQueue;
 
+//----------------------------
+WebServer server(80);
+
+const char* host = "EnRav";
+
+void returnOK() {
+  server.send(200, "text/plain", "");
+}
+
+void returnFail(String msg) {
+  server.send(500, "text/plain", msg + "\r\n");
+}
+
+bool loadFromSdCard(String path){
+  String dataType = "text/plain";
+  if(path.endsWith("/")) path += "index.htm";
+
+  if(path.endsWith(".src")) path = path.substring(0, path.lastIndexOf("."));
+  else if(path.endsWith(".htm")) dataType = "text/html";
+  else if(path.endsWith(".css")) dataType = "text/css";
+  else if(path.endsWith(".js")) dataType = "application/javascript";
+  else if(path.endsWith(".png")) dataType = "image/png";
+  else if(path.endsWith(".gif")) dataType = "image/gif";
+  else if(path.endsWith(".jpg")) dataType = "image/jpeg";
+  else if(path.endsWith(".ico")) dataType = "image/x-icon";
+  else if(path.endsWith(".xml")) dataType = "text/xml";
+  else if(path.endsWith(".pdf")) dataType = "application/pdf";
+  else if(path.endsWith(".zip")) dataType = "application/zip";
+
+  File dataFile = SD.open(path.c_str());
+  if(dataFile.isDirectory()){
+    path += "/index.htm";
+    dataType = "text/html";
+    dataFile = SD.open(path.c_str());
+  }
+
+  if (!dataFile)
+    return false;
+
+  if (server.hasArg("download")) dataType = "application/octet-stream";
+
+  if (server.streamFile(dataFile, dataType) != dataFile.size()) {
+    ESP_LOGD(TAG, "Sent less data than expected!");
+  }
+
+  dataFile.close();
+  return true;
+}
+
+void printDirectory() {
+  if(!server.hasArg("dir")) return returnFail("BAD ARGS");
+  String path = server.arg("dir");
+  if(path != "/" && !SD.exists((char *)path.c_str())) return returnFail("BAD PATH");
+  File dir = SD.open((char *)path.c_str());
+  path = String();
+  if(!dir.isDirectory()){
+    dir.close();
+    return returnFail("NOT DIR");
+  }
+  dir.rewindDirectory();
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200, "text/json", "");
+  WiFiClient client = server.client();
+
+  server.sendContent("[");
+  for (int cnt = 0; true; ++cnt) {
+    File entry = dir.openNextFile();
+    if (!entry)
+    break;
+
+    String output;
+    if (cnt > 0)
+      output = ',';
+
+    output += "{\"type\":\"";
+    output += (entry.isDirectory()) ? "dir" : "file";
+    output += "\",\"name\":\"";
+#ifdef ESP8266
+    output += entry.name();
+#else
+    // Ignore '/' prefix
+    output += entry.name()+1;
+#endif
+    output += "\"";
+    output += "}";
+    server.sendContent(output);
+    entry.close();
+ }
+ server.sendContent("]");
+ // Send zero length chunk to terminate the HTTP body
+ server.sendContent("");
+ dir.close();
+}
+
+File uploadFile;
+
+void handleFileUpload(){
+  if(server.uri() != "/edit") return;
+  HTTPUpload& upload = server.upload();
+  if(upload.status == UPLOAD_FILE_START){
+    if(SD.exists((char *)upload.filename.c_str())) SD.remove((char *)upload.filename.c_str());
+    uploadFile = SD.open(upload.filename.c_str(), FILE_WRITE);
+    ESP_LOGD(TAG, "Upload: START, filename: %s", upload.filename.c_str());
+  } else if(upload.status == UPLOAD_FILE_WRITE){
+    if(uploadFile) uploadFile.write(upload.buf, upload.currentSize);
+    ESP_LOGD(TAG, "Upload: WRITE, Bytes: %u", upload.currentSize);
+  } else if(upload.status == UPLOAD_FILE_END){
+    if(uploadFile) uploadFile.close();
+    ESP_LOGD(TAG, "Upload: END, Size: %u", upload.totalSize);
+  }
+}
+
+void deleteRecursive(String path){
+  File file = SD.open((char *)path.c_str());
+  if(!file.isDirectory()){
+    file.close();
+    SD.remove((char *)path.c_str());
+    return;
+  }
+
+  file.rewindDirectory();
+  while(true) {
+    File entry = file.openNextFile();
+    if (!entry) break;
+    String entryPath = path + "/" +entry.name();
+    if(entry.isDirectory()){
+      entry.close();
+      deleteRecursive(entryPath);
+    } else {
+      entry.close();
+      SD.remove((char *)entryPath.c_str());
+    }
+    yield();
+  }
+
+  SD.rmdir((char *)path.c_str());
+  file.close();
+}
+
+void handleDelete(){
+  if(server.args() == 0) return returnFail("BAD ARGS");
+  String path = server.arg(0);
+  if(path == "/" || !SD.exists((char *)path.c_str())) {
+    returnFail("BAD PATH");
+    return;
+  }
+  deleteRecursive(path);
+  returnOK();
+}
+
+void handleCreate(){
+  if(server.args() == 0) return returnFail("BAD ARGS");
+  String path = server.arg(0);
+  if(path == "/" || SD.exists((char *)path.c_str())) {
+    returnFail("BAD PATH");
+    return;
+  }
+
+  if(path.indexOf('.') > 0){
+    File file = SD.open((char *)path.c_str(), FILE_WRITE);
+    if(file){
+#ifdef ESP8266
+      file.write((const char *)0);
+#else
+      // TODO Create file with 0 bytes???
+      file.write(NULL, 0);
+#endif
+      file.close();
+    }
+  } else {
+    SD.mkdir((char *)path.c_str());
+  }
+  returnOK();
+}
+
+
+void handleNotFound(){
+  if(loadFromSdCard(server.uri())) return;
+  String message = "SDCARD Not Detected\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " NAME:"+server.argName(i) + "\n VALUE:" + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  ESP_LOGD(TAG, "%s", message.c_str());
+}
+
+//----------------------------
+
+
+
+
 
 //The setup function is called once at startup of the sketch
 void setup() {
@@ -56,11 +259,44 @@ void setup() {
 
     MyPlayer.begin(&PlayerCommandQueue);
 
-    // WiFi.disconnect();
-    // WiFi.mode(WIFI_STA);
-    // WiFi.begin(ssid.c_str(), password.c_str());
-    // while (WiFi.status() != WL_CONNECTED) delay(1500);
+//----------------------------
+    WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid.c_str(), password.c_str());
+    
+      // Wait for connection
+    uint8_t i = 0;
+    while (WiFi.status() != WL_CONNECTED && i++ < 20) //wait 10 seconds
+    {
+        delay(500);
+    }
 
+    if(i == 21)
+    {
+        ESP_LOGD(TAG, "Could not connect to %s", ssid.c_str());
+        while(1) delay(500);
+    }
+
+    ESP_LOGI(TAG, "Connected! IP address: %s", WiFi.localIP().toString().c_str());
+
+    if (MDNS.begin(host)) 
+    {
+        MDNS.addService("http", "tcp", 80);
+        ESP_LOGD(TAG, "MDNS responder started");
+        ESP_LOGD(TAG, "You can now connect to http://%s.local", host);
+    }
+
+
+    server.on("/list", HTTP_GET, printDirectory);
+    server.on("/edit", HTTP_DELETE, handleDelete);
+    server.on("/edit", HTTP_PUT, handleCreate);
+    server.on("/edit", HTTP_POST, [](){ returnOK(); }, handleFileUpload);
+    server.onNotFound(handleNotFound);
+
+    server.begin();
+    ESP_LOGI(TAG, "HTTP server started");
+
+//----------------------------
 
 }
 
@@ -68,164 +304,7 @@ void setup() {
 // The loop function is called in an endless loop
 void loop()
 {
-    // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
-    // MFRC522::MIFARE_Key key;
-    // for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
-
-
-    delay(100);
-
-
-
-
-
-//     // Check for compatibility
-//     if (    piccType != MFRC522::PICC_TYPE_MIFARE_MINI
-//         &&  piccType != MFRC522::PICC_TYPE_MIFARE_1K
-//         &&  piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-//         Serial.println(F("This sample only works with MIFARE Classic cards."));
-//         goto CleanUp;
-//     }
-// {
-//     // In this sample we use the second sector,
-//     // that is: sector #1, covering block #4 up to and including block #7
-//     byte sector         = 1;
-//     byte blockAddr      = 4;
-//     byte dataBlock[]    = {
-//         0x01, 0x02, 0x03, 0x04, //  1,  2,   3,  4,
-//         0x05, 0x06, 0x07, 0x08, //  5,  6,   7,  8,
-//         0x09, 0x0a, 0xff, 0x0b, //  9, 10, 255, 11,
-//         0x0c, 0x0d, 0x0e, 0x0f  // 12, 13, 14, 15
-//     };
-//     byte trailerBlock   = 7;
-//     MFRC522::StatusCode status;
-//     byte buffer[18];
-//     byte size = sizeof(buffer);
-
-//     // Authenticate using key A
-//     Serial.println(F("Authenticating using key A..."));
-//     status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
-//     if (status != MFRC522::STATUS_OK) {
-//         Serial.print(F("PCD_Authenticate() failed: "));
-//         Serial.println(mfrc522.GetStatusCodeName(status));
-//         return;
-//     }
-
-//     // Show the whole sector as it currently is
-//     Serial.println(F("Current data in sector:"));
-//     mfrc522.PICC_DumpMifareClassicSectorToSerial(&(mfrc522.uid), &key, sector);
-//     Serial.println();
-
-//     // Read data from the block
-//     Serial.print(F("Reading data from block ")); Serial.print(blockAddr);
-//     Serial.println(F(" ..."));
-//     status = mfrc522.MIFARE_Read(blockAddr, buffer, &size);
-//     if (status != MFRC522::STATUS_OK) {
-//         Serial.print(F("MIFARE_Read() failed: "));
-//         Serial.println(mfrc522.GetStatusCodeName(status));
-//     }
-//     Serial.print(F("Data in block ")); Serial.print(blockAddr); Serial.println(F(":"));
-//     dump_byte_array(buffer, 16); Serial.println();
-//     Serial.println();
-
-//     //prepare a dummy Tag
-//     memset(myTag.Target.DataPath, 0, sizeof(myTag.Target.DataPath));
-//     strcpy(myTag.Target.DataPath, "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod");
-
-//     ESP_LOGD(TAG, "Writing data into block 4..6, 8..10");
-
-//     ESP_LOGD(TAG, "writing info block 4 ..");
-//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(4, myTag.Information.Raw, 16);
-//     if (status != MFRC522::STATUS_OK) {
-//         Serial.print(F("MIFARE_Write(4) failed: "));
-//         Serial.println(mfrc522.GetStatusCodeName(status));
-//     }
-//     ESP_LOGD(TAG, "done");
-
-//     ESP_LOGD(TAG, "writing info block 5 ..");
-//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(5, myTag.Target.Raw[0], 16);
-//     if (status != MFRC522::STATUS_OK) {
-//         Serial.print(F("MIFARE_Write(5) failed: "));
-//         Serial.println(mfrc522.GetStatusCodeName(status));
-//     }
-//     ESP_LOGD(TAG, "done");
-
-//     ESP_LOGD(TAG, "writing info block 6 ..");
-//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(6, myTag.Target.Raw[1], 16);
-//     if (status != MFRC522::STATUS_OK) {
-//         Serial.print(F("MIFARE_Write(5) failed: "));
-//         Serial.println(mfrc522.GetStatusCodeName(status));
-//     }
-//     ESP_LOGD(TAG, "done");
-
-//     // Authenticate using key A
-//     Serial.println(F("Authenticating using key A..."));
-//     status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 10, &key, &(mfrc522.uid));
-//     if (status != MFRC522::STATUS_OK) {
-//         Serial.print(F("PCD_Authenticate() failed: "));
-//         Serial.println(mfrc522.GetStatusCodeName(status));
-//         return;
-//     }
-
-//     ESP_LOGD(TAG, "writing info block 8 ..");
-//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(8, myTag.Target.Raw[2], 16);
-//     if (status != MFRC522::STATUS_OK) {
-//         Serial.print(F("MIFARE_Write(8) failed: "));
-//         Serial.println(mfrc522.GetStatusCodeName(status));
-//     }
-//     ESP_LOGD(TAG, "done");
-
-//     ESP_LOGD(TAG, "writing info block 9 ..");
-//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(9, myTag.Target.Raw[3], 16);
-//     if (status != MFRC522::STATUS_OK) {
-//         Serial.print(F("MIFARE_Write(9) failed: "));
-//         Serial.println(mfrc522.GetStatusCodeName(status));
-//     }
-//     ESP_LOGD(TAG, "done");
-
-//             ESP_LOGD(TAG, "writing info block 10 ..");
-//     status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(10, myTag.Target.Raw[4], 16);
-//     if (status != MFRC522::STATUS_OK) {
-//         Serial.print(F("MIFARE_Write(10) failed: "));
-//         Serial.println(mfrc522.GetStatusCodeName(status));
-//     }
-//     ESP_LOGD(TAG, "done");
-    // PlayerControlMessage_s myMessage = { .Command = CMD_UNKNOWN };
-
-    // if (mfrc522.uid.size >= 2) 
-    // {
-    //     if ((mfrc522.uid.uidByte[0] == 0x04) && (mfrc522.uid.uidByte[1] == 0x65))
-    //     {
-    //         myMessage.Command       = CMD_PLAY_FILE;
-    //         myMessage.pFileToPlay   = (char *) malloc (16 * sizeof(char));
-    //         strncpy(myMessage.pFileToPlay, "/01.mp3", 16); 
-    //     }
-    //     else if ((mfrc522.uid.uidByte[0] == 0x04) && (mfrc522.uid.uidByte[1] == 0xE8))
-    //     {
-    //         myMessage.Command       = CMD_PLAY_FILE;
-    //         myMessage.pFileToPlay   = (char *) malloc (16 * sizeof(char));
-    //         strncpy(myMessage.pFileToPlay, "/02.mp3", 16); 
-    //     }
-    //     else if ((mfrc522.uid.uidByte[0] == 0x04) && (mfrc522.uid.uidByte[1] == 0x01))
-    //     {
-    //         myMessage.Command       = CMD_STOP;
-    //         myMessage.pFileToPlay   = NULL;
-    //     }
-    // }
-
-    // if (myMessage.Command != CMD_UNKNOWN) 
-    // {
-    //     if (xQueueSend( *pPlayerQueue, &myMessage, ( TickType_t ) 0 ) )
-    //     {
-    //         ESP_LOGD(TAG, "send to queue successfull");
-    //     } else {
-    //         ESP_LOGD(TAG, "send to queue failed");
-
-    //         //if the send failed, we must do the job
-    //         free(myMessage.pFileToPlay);
-    //     }
-    // }
-// }
-
+    server.handleClient();
+    // delay(100);
 
 }
