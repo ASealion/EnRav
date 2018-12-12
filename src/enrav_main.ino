fad435f5211676ba6173083e7946055db803ff4e
EnRav <developer@mail.enrav.de>
2018-12-12 21:53:38 +0100

    move pin configuration to separate header file

--- src/enrav_main.ino
+++ src/enrav_main.ino
@@ -12,26 +12,12 @@
 #endif
 
 #include "credentials.h"
+#include "pinout.h"
 
 #ifndef __CREDENTIALS__H
     String ssid =     "WLAN";
     String password = "xxxxxxxxxxxxxxxx";
 #endif
-
-// Digital I/O used
-#define SPI_CLK         18    
-#define SPI_MISO        19
-#define SPI_MOSI        23
-
-#define SDCARD_CS       13
-
-#define VS1053_CS       25
-#define VS1053_DCS      26
-#define VS1053_DREQ     32
-
-#define MFRC522_RST      5
-#define MFRC522_CS      21
-
 
 int volume=15;
 
---
+++ src/pinout.h
@@ -0,0 +1,18 @@
+#ifndef __PINOUT__H
+    #define __PINOUT__H
+
+	// Digital I/O used
+	#define SPI_CLK         18    
+	#define SPI_MISO        19
+	#define SPI_MOSI        23
+
+	#define SDCARD_CS       13
+
+	#define VS1053_CS       25
+	#define VS1053_DCS      26
+	#define VS1053_DREQ     32
+
+	#define MFRC522_RST      5
+	#define MFRC522_CS      21
+
+#endif
